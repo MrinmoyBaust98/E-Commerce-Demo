8fbd6555da2df21c366a392d334b14c2e0bd0bf0
MrinmoyBaust98 <baust.cse.170201073@gmail.com>
2023-02-17 02:37:20 +0600

    font assome rating added

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Products.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import Rating from 'react-rating';
 
 const Products = (props) => {
-    // console.log(props.product)
-    const{img,name,seller,price,stock}=props.product;
+    // console.log(props)
+    const{img,name,seller,price,stock,star}=props.product;
     const element = <FontAwesomeIcon icon={faShoppingCart } />
     return (
         <div className='products'>
@@ -17,6 +18,15 @@ const Products = (props) => {
                 <p> By:<small> {seller}</small></p>
                 <p> Price: ${price}</p>
                 <p> Item left: {stock}</p>
+                <Rating
+                initialRating={star}
+                readonly
+                   emptySymbol=" fa-sharp fa-regular fa-star rating-style"
+                   fullSymbol=" fa-sharp fa-solid fa-star rating-style"
+                />
+                
+                <br />
+
                 <button onClick={()=>props.HandleAddToCart(props.product)} className='purches-btn'>{element} add to cart </button>
             </div>
             
